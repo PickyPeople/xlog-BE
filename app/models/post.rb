03a7445ed97561b357c3e29bfad3de22c7e0c8a3
pickyPeople <jjyjjh33@gmail.com>
2025-02-19 20:53:58 +0900

    chore: コードの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :image  # Active Storage 설정
+  has_one_attached :image  
 
   validates :title, presence: true
   validates :sub, presence: true
   validates :date, presence: true
-  validates :content, presence: true  # content 필드 검증 추가
+  validates :content, presence: true  
   validates :likeNum, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def as_json(options = {})
     super(options).merge({
       image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil,
-      username: user.username # 사용자 이름도 함께 포함
+      username: user.username 
     })
   end
 end
