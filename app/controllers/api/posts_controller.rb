641463cbf1edf63626650fe8a97af78914dece79
pickyPeople <jjyjjh33@gmail.com>
2025-02-19 21:32:48 +0900

    feat: tagのデータベース追加

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -10,11 +10,18 @@ module Api
     end
  
     def create
-      @post = Post.new(post_params)
+      @post = Post.new(post_params.except(:tags))
       @post.user = current_user
       @post.date = Date.today
       
       if @post.save
+        # post가 저장된 후에 태그 처리
+        if params[:post][:tags].present?
+          params[:post][:tags].each do |tag_name|
+            tag = Tag.find_or_create_by(name: tag_name)
+            @post.tags << tag
+          end
+        end
         render json: @post, status: :created
       else
         render json: { error: @post.errors.full_messages }, status: :unprocessable_entity
@@ -28,6 +35,14 @@ module Api
     end
  
     def update
+      if params[:post][:tags].present?
+        @post.tags.clear  # 기존 태그 삭제
+        params[:post][:tags].each do |tag_name|
+          tag = Tag.find_or_create_by(name: tag_name)
+          @post.tags << tag
+        end
+      end
+ 
       if @post.update(post_params)
         render json: @post
       else
@@ -49,7 +64,7 @@ module Api
     end
  
     def post_params
-      params.require(:post).permit(:title, :sub, :content, :image)
+      params.require(:post).permit(:title, :sub, :content, :image, tags: [])
     end
  
     def check_post_owner
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,24 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :image  
-
-  validates :title, presence: true
-  validates :sub, presence: true
-  validates :date, presence: true
-  validates :content, presence: true  
-  validates :likeNum, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  has_one_attached :image
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 
   def as_json(options = {})
-    super(options).merge({
+    json = super(options)
+    json.merge!({
       image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil,
-      username: user.username 
+      username: user&.username || '',
+      tags: []  # 기본값으로 빈 배열 설정
     })
+
+    # tags 관계가 존재하는 경우에만 pluck 실행
+    begin
+      json[:tags] = tags.pluck(:name) if self.respond_to?(:tags)
+    rescue => e
+      Rails.logger.error("태그 정보 가져오기 실패: #{e.message}")
+    end
+
+    json
   end
 end
